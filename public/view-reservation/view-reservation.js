955c6ed46af368ff91ad47f522843321be214977
Mrwan Zein <mrwanzein@outlook.com>
2020-07-16 12:31:26 -0400

    User info displays in view-reservation

--- public/view-reservation/view-reservation.js
+++ public/view-reservation/view-reservation.js
@@ -1,6 +1,17 @@
+let userFlightNumber = document.getElementById('user-flight-number');
+let userSeatNumber = document.getElementById('user-seat-number');
+let userFullName = document.getElementById('user-full-name');
+let userEmail = document.getElementById('user-email');
+
 let userData;
 
 fetch('/get-user-info')
 .then(res => res.json())
-.then(data => userData = data)
+.then(data => {
+    userData = data
+    userFlightNumber.innerHTML = userData.flight;
+    userSeatNumber.innerHTML = userData.seat;
+    userFullName.innerHTML = `${userData.givenName} ${userData.surname}`;
+    userEmail.innerHTML = userData.email;
+})
 .catch((err) => console.log(err));
